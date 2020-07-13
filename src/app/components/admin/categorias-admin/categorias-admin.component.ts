933cff133d57884176de8a37909cb6c45a4291d1
Maria Gabriela <cafarelli172006@gmail.com>
2020-07-12 22:21:05 -0400

    crud categorias vista

--- src/app/components/admin/categorias-admin/categorias-admin.component.ts
+++ src/app/components/admin/categorias-admin/categorias-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WaveServiceService } from 'src/app/services/wave-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-categorias-admin',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorias-admin.component.scss']
 })
 export class CategoriasAdminComponent implements OnInit {
+  categories: any;
+  files: File[] = [];
 
-  constructor() { }
+  constructor(
+    private waveService: WaveServiceService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.waveService.getCategoriesWSubcategories().subscribe((response) => {
+      this.categories = response.items;
+      console.log('categorias', this.categories);
+    });
+  }
+
+  onSelect(event) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+
+  onRemove(event) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
   }
 
 }
