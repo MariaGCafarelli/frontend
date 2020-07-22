90f6ad7d5e9f75069297c27bd5385d9a7490eeae
Rafael Urdaneta <rafael.urdaneta@correo.unimet.edu.ve>
2020-07-22 13:21:21 -0400

    usuarios y admins

--- src/app/components/admin/usuarios-admin/usuarios-admin.component.ts
+++ src/app/components/admin/usuarios-admin/usuarios-admin.component.ts
@@ -11,12 +11,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class UsuariosAdminComponent implements OnInit {
-Admins:any[]=[];
+
 users:any[] = []; 
 currentPage: number = 1;
 nextPage: boolean;
-currentPage2: number = 1;
-nextPage2: boolean;
+
 
   constructor(private waveService: WaveServiceService) { }
 
@@ -29,30 +28,15 @@ nextPage2: boolean;
        this.currentPage !== parseInt(res.users.meta.totalPages);
      })
 
-     this.waveService.getAdmins(this.currentPage2).subscribe((res)=>{
-      console.log(res)
-      this.Admins = res.admins.items;
-      console.log(this.users);
-       this.currentPage2 = parseInt(res.admins.meta.currentPage);
-       this.nextPage2 =
-       this.currentPage2 !== parseInt(res.admins.meta.totalPages);
-    })
+     
   }
 
- estadoAdmin(email: string){
-    this.waveService.statusAdmin(email).subscribe((res)=>{
-      console.log(res);
-      this.waveService.getAdmins().subscribe((res)=>{
-        console.log(res)
-        this.Admins = res;
-      })
-    })
- }
+
 
  estadoUser(email: string){
   this.waveService.statusNormal(email).subscribe((res)=>{
     console.log(res);
-    this.waveService.getNormalUsers(this.currentPage).subscribe((res)=>{
+    this.waveService.getNormalUsers().subscribe((res)=>{
       console.log(res)
       this.users = res.users.items;
     })
@@ -70,16 +54,7 @@ traerMasUsers() {
     });
 }
 
-traerMasAdmins() {
-  this.waveService
-    .getAdmins(this.currentPage2 + 1)
-    .subscribe((response) => {
-      this.Admins = this.Admins.concat(response.admins.items);
-      this.currentPage2 = parseInt(response.admins.meta.currentPage);
-      this.nextPage2 = this.currentPage2 !== parseInt(response.admins.meta.totalPages);
-      
-    });
-}
+
 
 }
 
